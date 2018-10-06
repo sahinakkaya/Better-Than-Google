20448ac8f122b0f56e4c9bb256a8f7ebea02bf8c
Asocia <akkayas17@itu.edu.tr>
2018-10-06 23:53:30 +0300

    Fix some lines that causes a bug

--- patterns.py
+++ patterns.py
@@ -11,19 +11,19 @@ p_changed = re.compile(
 p_removed = re.compile(
     r"(?P<MAIN>.*)(?: removed )(?<!.* (?:created group \"|added |changed the subject from \").*)(?<!.*:.*)(?P<OTHER>.*)")
 p_created = re.compile(
-    r"(?P<MAIN>.*)(?: created group \")(?<!.* (?:added |removed |changed the subject from \").*)(?<!.*:.*)(?P<OTHER>.*)\"$")
+    r"(?P<MAIN>.*)(?: created group \")(?<!.* (?:added |removed |changed the subject from \").*)(?<!.*:.*)(.*)\"$")
 p_left = re.compile(r"(?P<MAIN>.*)(?: left$)(?<!.* (?:created group \"|removed |added ).*)(?<!.*: .*)")
 p_security = re.compile(
     r"(?P<MAIN>.*)(?:'s security code changed. Tap for more info.$)(?<!.* (?:created group \"|removed |added ).*)(?<!.*: .*)")
 p_already_added = re.compile(
     r"(?P<MAIN>.*)(?<!.* (?:added ).*)(?: was added$)(?<!.* (?:created group \"|removed ).*)(?<!.*: .*)")
 p_joined = re.compile(r"(?P<MAIN>.*)(?: joined using this group's invite link$)(?<!.*: .*)")
-p_change_su = re.compile(r"(?P<MAIN>.*)(?: changed the subject from \")(?<!.*: .*)(?P<OTHER>.*)\" to \"(?P<OTHER2>.*)\"$")
+p_change_su = re.compile(r"(?P<MAIN>.*)(?: changed the subject from \")(?<!.*: .*)(.*)\" to \"(.*)\"$")
 p_change_se = re.compile(
     r"(?P<MAIN>.*)(?: changed this group's settings to allow only admins to edit this group's info$)(?<!.*: .*)")
 p_change_i = re.compile(r"(?P<MAIN>.*)(?: changed this group's icon$)(?<!.*: .*)")
 p_change_d = re.compile(r"(?P<MAIN>.*)(?: changed the group description$)(?<!.*: .*)")
-p_message = re.compile(r"(?P<MAIN>.*): (.*)")
+p_message = re.compile(r"(?P<MAIN>.*?): (.*)")
 operations = {p_added: 'AddPerson', p_removed: 'RemovePerson', p_created: 'CreateGroup',
               p_left: 'LeftGroup', p_joined: 'JoinedGroup', p_change_se: "ChangeGroupSettings",
               p_change_i: 'ChangeGroupIcon', p_already_added: 'AlreadyAdded', p_message: "SendMessage",
