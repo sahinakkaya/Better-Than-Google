49dfb559ba57a50e26f4fb077397d133e471f27d
Asocia <akkayas17@itu.edu.tr>
2018-10-11 22:45:09 +0300

    Reformat the code accordance to PEP8

--- test_chat.py
+++ test_chat.py
@@ -4,16 +4,17 @@ from parser import Chat, Line
 import io
 from datetime import datetime
 
+
 class TestChat(unittest.TestCase):
     @mock.patch('sys.stdout', new_callable=io.StringIO)
     @mock.patch('builtins.input')
-    def test_group_chat(self,fake_stdin, fake_stdout):
+    def test_group_chat(self, fake_stdin, fake_stdout):
         fake_stdin.return_value = "Şahin Akkaya"
         self.chat = Chat("chats/itu")
-        self.assertEqual(self.chat.title_history[-1],"ITU COMPUTER 17-18")
-        self.assertEqual(self.chat.type,"Group Chat")
-        self.assertEqual(self.chat.right_side_person,"Şahin Akkaya")
-        self.assertEqual(len(self.chat.persons['Şahin Akkaya'].existence),2)
+        self.assertEqual(self.chat.title_history[-1], "ITU COMPUTER 17-18")
+        self.assertEqual(self.chat.type, "Group Chat")
+        self.assertEqual(self.chat.right_side_person, "Şahin Akkaya")
+        self.assertEqual(len(self.chat.persons['Şahin Akkaya'].existence), 2)
         # self.assertTrue(all(self.chat.persons[p].existence[0][0] is not None for p in self.chat.persons))
         # self.assertTrue(all(self.chat.persons[p].existence[-1][1] is not None for p in self.chat.persons))
 
@@ -25,9 +26,7 @@ class TestChat(unittest.TestCase):
         self.assertEqual(self.chat.title_history[-1], "Şahin Akkaya")
         self.assertEqual(self.chat.type, "2 Person Chat")
         self.assertEqual(self.chat.right_side_person, "Babam")
-        self.assertListEqual(self.chat.persons['Babam'].existence, [[None,None]])
-
-
+        self.assertListEqual(self.chat.persons['Babam'].existence, [[None, None]])
 
     def test_create(self):
         line = Line('9/16/17, 9:06 PM - ‪+90 534 317 04 77‬ created group "İTÜ BİLGİSAYAR 17-18"‬‬')
@@ -37,12 +36,11 @@ class TestChat(unittest.TestCase):
         self.assertSetEqual(line.affected_persons, set())
         self.assertSetEqual(line.main_persons, {"+90 534 317 04 77"})
 
-
     def test_send_message(self):
         line = Line("10/6/17, 6:11 PM - ‪+90 506 369 83 17‬: Merhaba")
         self.assertEqual(line.text, "10/6/17, 6:11 PM - +90 506 369 83 17: Merhaba")
-        self.assertEqual(line.operation,"SendMessage")
-        self.assertEqual(line.time,datetime(2017,10,6,18,11))
+        self.assertEqual(line.operation, "SendMessage")
+        self.assertEqual(line.time, datetime(2017, 10, 6, 18, 11))
         self.assertSetEqual(line.extract_persons("OTHER"), set())
         self.assertSetEqual(line.extract_persons(), {"+90 506 369 83 17"})
 
@@ -51,7 +49,7 @@ class TestChat(unittest.TestCase):
         self.assertEqual(line.text, "4/10/18, 15:54 - +90 554 643 08 24 added +90 531 226 92 68 and +90 544 534 33 90")
         self.assertEqual(line.operation, "AddPerson")
         self.assertEqual(line.time, datetime(2018, 4, 10, 15, 54))
-        self.assertSetEqual(line.affected_persons, {"+90 531 226 92 68","+90 544 534 33 90"})
+        self.assertSetEqual(line.affected_persons, {"+90 531 226 92 68", "+90 544 534 33 90"})
         self.assertSetEqual(line.main_persons, {"+90 554 643 08 24"})
 
         line = Line("4/10/18, 15:54 - Şahin Akkaya‬ added person name with colon : (Weird person)‬")
@@ -64,7 +62,7 @@ class TestChat(unittest.TestCase):
         self.assertEqual(line.text, "10/26/17, 2:42 PM - +90 534 317 04 77 removed +90 546 402 31 26")
         self.assertEqual(line.operation, "RemovePerson")
         self.assertEqual(line.time, datetime(2017, 10, 26, 14, 42))
-        self.assertSetEqual(line.affected_persons,{"+90 546 402 31 26"})
+        self.assertSetEqual(line.affected_persons, {"+90 546 402 31 26"})
         self.assertSetEqual(line.main_persons, {"+90 534 317 04 77"})
 
     def test_left(self):
@@ -80,6 +78,7 @@ class TestChat(unittest.TestCase):
         self.assertEqual(line.time, datetime(2017, 10, 6, 18, 6))
         self.assertSetEqual(line.affected_persons, set())
         self.assertSetEqual(line.main_persons, {"You"})
+
 
 if __name__ == '__main__':
     unittest.main()
